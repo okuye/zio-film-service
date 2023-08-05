8f8cdfb44a840b7f317c1b144cf187370fdcb75b
okuye <olakuye@gmail.com>
2023-08-05 14:15:58 +0100

    Fixed duplicates (#3)

--- src/main/scala/com/thirdparty/movie/MovieService 2.scala
+++
@@ -1,8 +0,0 @@
-package com.thirdparty.movie
-
-import com.sky.Exception.CustomException
-import zio.IO
-
-trait MovieService {
-  def getParentalControlLevel(movieId: String): IO[CustomException, String]
-}
